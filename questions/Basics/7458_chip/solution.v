99a3a82c95ce76422dcb11c49ee3a203d9d0bdfc
pulkit042 <yadav.pulkit0712@gmail.com>
2025-04-26 12:15:48 +0530

    Update solution.v

--- questions/Basics/7458_chip/solution.v
+++ questions/Basics/7458_chip/solution.v
@@ -15,12 +15,12 @@ wire and_result3, and_result4;   // For first part (p1y)
 assign and_result1 = p2a & p2b;   // AND of p2a and p2b
 assign and_result2 = p2c & p2d;   // AND of p2c and p2d
 
-assign p2y = and_result1 | and_result2;   // OR the two results to produce output p2y
+assign p2y = and_result1 || and_result2;   // OR the two results to produce output p2y
 
 // Logic for first part (p1y)
 assign and_result3 = p1a & p1b & p1c; // AND of p1a, p1b, and p1c
 assign and_result4 = p1d & p1e & p1f; // AND of p1d, p1e, and p1f
 
-assign p1y = and_result3 | and_result4;   // OR the two results to produce output p1y
+assign p1y = and_result3 || and_result4;   // OR the two results to produce output p1y
 
 endmodule
